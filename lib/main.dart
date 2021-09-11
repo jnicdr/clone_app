36198584693f64eef1ec23662845541d02886b69
DRJNIC007 <olamigokejonny@gmail.com>
2021-09-10 22:37:02 -1200

    commit on 'master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:clone_app/screens/greeting.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:clone_app/screens/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -6,10 +7,40 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100],
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'COFFEE WEY CHOKE',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 40),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                      EdgeInsets.symmetric(horizontal: 130, vertical: 15))),
+              child: Text('Sign in Anon'),
+              onPressed: () async {
+                dynamic result = await _auth.signInAnon();
+                if (result == null) {
+                  print('error signing in');
+                } else {
+                  print('signed in');
+                  print(result);
+                }
+              },
+            ),
+          )
+        ],
+      )),
     );
   }
 }
